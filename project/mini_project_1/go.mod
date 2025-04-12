7750788ed9d24ad402a9649ba68f1e344f856496
eduardoguedes <eduardo.gomero@fbm.pt>
2025-04-12 20:47:30 +0100

    add project 1

---
+++ project/mini_project_1/go.mod
@@ -0,0 +1,3 @@
+module github.com/dostini/enei-golang-workshop/project/mini_project_1
+
+go 1.24.2
---
+++ project/mini_project_1/main_test.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestIterateOverUrlSync(t *testing.T) {
+	urls := []string{
+		"https://www.google.com",
+		"https://www.facebook.com",
+		"https://www.example.com",
+		"https://www.twitter.com",
+		"https://blog.gypsydave5.com",
+	}
+	expected := map[string]bool{
+		"https://www.google.com":      true,
+		"https://www.facebook.com":    true,
+		"https://www.example.com":     true,
+		"https://blog.gypsydave5.com": true,
+		"https://www.twitter.com":     false,
+	}
+
+	got := iterateOverUrlSync(urls)
+
+	for key, value := range expected {
+		if got[key] != value {
+			t.Errorf("got %+v, expected %+v", got[key], value)
+		}
+	}
+}
+
+func TestIterateOverUrlAsync(t *testing.T) {
+	urls := []string{
+		"https://www.google.com",
+		"https://www.facebook.com",
+		"https://www.example.com",
+		"https://www.twitter.com",
+		"https://blog.gypsydave5.com",
+	}
+	expected := map[string]bool{
+		"https://www.google.com":      true,
+		"https://www.facebook.com":    true,
+		"https://www.example.com":     true,
+		"https://blog.gypsydave5.com": true,
+		"https://www.twitter.com":     false,
+	}
+
+	got := iterateOverUrlAsync(urls)
+
+	for key, value := range expected {
+		if got[key] != value {
+			t.Errorf("got %+v, expected %+v", got[key], value)
+		}
+	}
+}
+func TestIterateSyncWithSleep(t *testing.T) {
+	iterateSyncWithSleep()
+}
+
+func TestIterateAsyncWithSleepWithoutWaitGroup(t *testing.T) {
+	iterateAsyncWithSleepWithoutWaitGroup()
+}
+
+func TestIterateAsyncWithSleepWithWaitGroup(t *testing.T) {
+	iterateAsyncWithSleepWithWaitGroup()
+}
